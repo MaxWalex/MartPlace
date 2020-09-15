ca13bd3209532316f12f6cd66292fad8df48e2bc
MaxWalex <chervonoshtan02@gmail.com>
2020-09-15 22:13:33 +0300

    release

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,8 @@ gulp.task("js", function () {
 gulp.task('script', function() {
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
-    "node_modules/rateyo/src/jquery.rateyo.js"
+    "node_modules/rateyo/src/jquery.rateyo.js",
+    "node_modules/mixitup/dist/mixitup.js",
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
