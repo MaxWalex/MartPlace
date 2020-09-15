b90d8327969b6460b17465370000ef14179a3509
MaxWalex <chervonoshtan02@gmail.com>
2020-09-15 19:40:59 +0300

    featured

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ gulp.task("style", function () {
     .src([
       "node_modules/normalize.css/normalize.css",
       "node_modules/slick-carousel/slick/slick.css",
+      "node_modules/rateyo/src/jquery.rateyo.css"
     ])
     .pipe(concat("_lins.min.css"))
     .pipe(cssmin())
@@ -56,7 +57,8 @@ gulp.task("js", function () {
 
 gulp.task('script', function() {
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
+    'node_modules/slick-carousel/slick/slick.js',
+    "node_modules/rateyo/src/jquery.rateyo.js"
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
